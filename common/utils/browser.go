d5dff0fe7549080330ec4a36c2fb77b1f50da216
GFW-knocker <123458121+GFW-knocker@users.noreply.github.com>
2026-02-27 14:39:19 +0330

    modify userAgent

--- common/utils/browser.go
+++ common/utils/browser.go
@@ -12,8 +12,8 @@ func ChromeVersion() int {
 	// Use only CPU info as seed for PRNG
 	seed := int64(cpuid.CPU.Family + cpuid.CPU.Model + cpuid.CPU.PhysicalCores + cpuid.CPU.LogicalCores + cpuid.CPU.CacheLine)
 	rng := rand.New(rand.NewSource(seed))
-	// Start from Chrome 144 released on 2026.1.13
-	releaseDate := time.Date(2026, 1, 13, 0, 0, 0, 0, time.UTC)
+	// Start from Chrome 144 available on 2026.1.27
+	releaseDate := time.Date(2026, 1, 27, 0, 0, 0, 0, time.UTC)
 	version := 144
 	now := time.Now()
 	// Each version has random 25-45 day interval
